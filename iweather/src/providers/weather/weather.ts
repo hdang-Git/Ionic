1ad37639e2cbccfaea2be102e3bb8decf472c8a7
hdang-Git <hdang0010@gmail.com>
2018-01-13 10:30:03 -0500

    initial commit

---
+++ iweather/src/providers/weather/weather.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+/*
+  Generated class for the WeatherProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class WeatherProvider {
+  apiKey = 'ae924bf1760105c4';
+  url;
+  constructor(public http: HttpClient) {
+    console.log('Hello WeatherProvider Provider');
+    this.url = 'http://api.wunderground.com/api/' + this.apiKey + '/conditions/q';
+  }
+
+  getWeather(city, state){
+      return this.http.get(this.url + '/'+ state + '/' + city + '.json')
+      .map(res => res);
+  }
+
+
+}
